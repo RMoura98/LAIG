ab66d477ccc7af9f5597673f1424993ed6ea9458
Tomas FIgueiredo <tomasviana_cof@hotmail.com>
2018-10-02 12:37:40 +0100

    same ol'

--- MySceneGraph.js
+++ MySceneGraph.js
@@ -414,7 +414,7 @@ class MySceneGraph {
             }
         }        
 
-        if ( (this.views[0].length + this.views[1].length) < 1)
+        if( (Object.keys(this.views[0]).length) + (Object.keys(this.views[1]).length) < 1 )
             return "at least one view (perspective or ortho) must be defined";
 
         this.log("Parsed views");
