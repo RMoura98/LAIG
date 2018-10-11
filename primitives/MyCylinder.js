1db59c39e6c4550f472ebd80966120e9dbebbfec
Tomas Figueiredo <tomasviana_cof@hotmail.com>
2018-10-11 21:46:07 +0100

    Somewhat updated cylinder primitive

--- primitives/MyCylinder.js
+++ primitives/MyCylinder.js
@@ -1,8 +1,13 @@
 class MyCylinder extends CGFobject
 {
-	constructor(scene, base, top, height, slices, stacks) 
+	constructor(scene, base, top, height, slices, stacks, minS, maxS, minT, maxT) 
 	{
 		super(scene);
+
+		this.minS = minS || 0.0;
+		this.maxS = maxS || 1.0;
+		this.minT = minT || 0.0;
+		this.maxT = maxT || 1.0;
 
 		this.baseRadius = base;
 		this.topRadius = top;
@@ -16,27 +21,31 @@ class MyCylinder extends CGFobject
 	initBuffers() 
 	{
 		this.vertices = [
-		];
+				];
 
 		this.indices = [
-		];
+			];
 		
 		this.normals = [
 		];
+
+		this.texCoords = [
+		];
 		
-
-		// TODO
-		// fix the display
-
+		//TODO
+		//fix the display
 		var angInt = (Math.PI*2)/this.slices;
 
 		for(var j = 0; j <= this.stacks; j++)
 		{
-			for(var i = 0; i < this.slices; i++)
+			for(var i = 0; i <= this.slices; i++)
 			{
-				this.vertices.push(this.baseRadius*Math.cos(angInt*i), this.baseRadius*Math.sin(angInt*i), j*(this.height/this.stacks));
+				this.vertices.push(this.baseRadius*Math.cos(angInt*i), this.baseRadius*Math.sin(angInt*i), 1-(j/this.stacks));
 
-				this.normals.push(this.baseRadius*Math.cos(angInt*i), this.baseRadius*Math.sin(angInt*i), j*(this.height/this.stacks));
+				this.normals.push(this.baseRadius*Math.cos(angInt*i), this.baseRadius*Math.sin(angInt*i), 1-(j/this.stacks));
+
+				this.texCoords.push(this.minS + i*((this.maxS - this.minS) / this.slices), this.minT + j*((this.maxT - this.minT) / this.stacks));
+
 			}
 		}
 
@@ -44,37 +53,19 @@ class MyCylinder extends CGFobject
 
 		for(var k=0; k < (this.slices*this.stacks); k++)
 		{
+
 			if( ((k+1)%this.slices) == 0)
 			{
-				this.indices.push(k+this.slices, k-this.slices+1, k);
-				this.indices.push(k-this.slices+1, k+this.slices, k+1);
+				this.indices.push(k+this.slices + 1, k+1, k);
+				this.indices.push(k+1, k+this.slices+1, k+this.slices+2);
 			}
 			else
 			{
-				this.indices.push(k + this.slices, k + 1, k);
-				this.indices.push(k + 1, k + this.slices, k + 1 + this.slices);
+				this.indices.push(k + this.slices + 1, k + 1, k);
+				this.indices.push(k + 1, k + this.slices + 1, k + 2 + this.slices);
 			}
 			
 		}
-
-		/*
-		for(var i = 0; i<this.slices; i++)
-		{
-			this.vertices.push(Math.cos(angInt*i), Math.sin(angInt*i), 0);
-			this.normals.push(0, 0, 1);
-			this.texCoords.push(0.5 + (Math.cos(angInt*i)/2), 0.5 - (Math.sin(angInt*i)/2));
-		}
-
-
-		for(var j = 1; j<(this.slices+1); j++)
-		{	
-			if( (j+1) == (this.slices+1) )
-				this.indices.push(0, j, 1);
-
-			else
-				this.indices.push(0, j, j+1);
-		}
-		*/
 		
 		this.primitiveType=this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
