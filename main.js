249841b4af0638debe647b70dadaf8bbd6d7bc9e
Unknown <up201604912@fe.up.pt>
2018-10-12 17:04:30 +0100

    juntei o cilindro ainda nao esta bem o cilindro mas esta melhor

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function getUrlVars() {
     return vars;
 }
 //Include additional files here
-serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js', './primitives/MyQuad.js', './primitives/MyCylinder.js', 'MyInterface.js', 'Node.js',
+serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js', './primitives/MyQuad.js', './primitives/MyCylinder.js', 'MyInterface.js', 'Node.js', './primitives/MyCircle.js',
 
 main=function()
 {
--- primitives/MyCylinder.js
+++ primitives/MyCylinder.js
@@ -1,7 +1,5 @@
-class MyCylinder extends CGFobject
-{
-	constructor(scene, base, top, height, slices, stacks, minS, maxS, minT, maxT)
-	{
+class MyCylinder extends CGFobject {
+	constructor(scene, base, top, height, slices, stacks, minS, maxS, minT, maxT) {
 		super(scene);
 
 		this.minS = minS || 0.0;
@@ -15,66 +13,76 @@ class MyCylinder extends CGFobject
 		this.slices = slices;
 		this.stacks = stacks;
 
+		this.base = new MyCircle(this.scene, this.slices);
+
 		this.initBuffers();
 	};
 
-	initBuffers()
-	{
-		this.vertices = [
-				];
+	initBuffers() {
+		this.vertices = [];
 
-		this.indices = [
-			];
+		this.indices = [];
 
-		this.normals = [
-		];
+		this.normals = [];
 
-		this.texCoords = [
-		];
+		this.texCoords = [];
 
 		//TODO
 		//fix the display
-		var angInt = (Math.PI*2)/this.slices;
+		var angInt = (Math.PI * 2) / this.slices;
 
-		for(var j = 0; j <= this.stacks; j++)
-		{
-			for(var i = 0; i <= this.slices; i++)
-			{
-				this.vertices.push(this.baseRadius*Math.cos(angInt*i), this.baseRadius*Math.sin(angInt*i), 1-(j/this.stacks));
+		for (var j = 0; j <= this.stacks; j++) {
+			for (var i = 0; i <= this.slices; i++) {
+				this.vertices.push(this.baseRadius * Math.cos(angInt * i), this.baseRadius * Math.sin(angInt * i), 1 - (j / this.stacks));
 
-				this.normals.push(this.baseRadius*Math.cos(angInt*i), this.baseRadius*Math.sin(angInt*i), 1-(j/this.stacks));
+				this.normals.push(this.baseRadius * Math.cos(angInt * i), this.baseRadius * Math.sin(angInt * i), 1 - (j / this.stacks));
 
-				this.texCoords.push(this.minS + i*((this.maxS - this.minS) / this.slices), this.minT + j*((this.maxT - this.minT) / this.stacks));
+				this.texCoords.push(this.minS + i * ((this.maxS - this.minS) / this.slices), this.minT + j * ((this.maxT - this.minT) / this.stacks));
 
 			}
 		}
 
 
-
-		for(var k=0; k < (this.slices*this.stacks); k++)
-		{
-
-			if( ((k+1)%this.slices) == 0)
-			{
-				this.indices.push(k+this.slices + 1, k+1, k);
-				this.indices.push(k+1, k+this.slices+1, k+this.slices+2);
+		for (let i = 0; i < this.stacks; i++) {
+			for (let j = 0; j < this.slices; j++) {
+				if (j == this.slices - 1) {
+					this.indices.push( (((i + 1) * this.slices + j) + 1) - this.slices,  (i * this.slices + j) + 1 - this.slices,(i * this.slices + j));
+					this.indices.push(((i + 1) * this.slices + j), (((i + 1) * this.slices + j) + 1) - this.slices, (i * this.slices + j));
+				}
+				else {
+					this.indices.push(((i + 1) * this.slices + j) + 1, (i * this.slices + j) + 1, (i * this.slices + j));
+					this.indices.push(((i + 1) * this.slices + j), ((i + 1) * this.slices + j) + 1,(i * this.slices + j) );
+				}
 			}
-			else
-			{
-				this.indices.push(k + this.slices + 1, k + 1, k);
-				this.indices.push(k + 1, k + this.slices + 1, k + 2 + this.slices);
-			}
-
 		}
 
-		this.primitiveType=this.scene.gl.TRIANGLES;
+
+		this.primitiveType = this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
 	};
+
+	display(){
+
+		super.display();
+		
+		//top base
+		this.scene.pushMatrix();
+			this.scene.scale(this.topRadius, this.topRadius, 1);
+			this.scene.translate(0, 0, 0.65*this.height); //qualquer coisa aqui nao esta bem...
+			this.base.display();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();
+			this.scene.scale(this.baseRadius, this.baseRadius, 1);
+			this.scene.rotate(Math.PI, 0, 1, 0);
+			this.base.display();
+		this.scene.popMatrix();
+	}
 
 	updateTexCoords(s, t) {
 		for (var i = 0; i < this.texCoords.length; i += 2) {
 			this.texCoords[i] = this.texCoords[i] / s;
-			this.texCoords[i + 1] = this.texCoords[i+1] / t;
+			this.texCoords[i + 1] = this.texCoords[i + 1] / t;
 		}
 		this.updateTexCoordsGLBuffers();
 	};
