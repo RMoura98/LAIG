628b927e3fa0fa40f9f7609647659519cf32e34a
Tomas FIgueiredo <tomasviana_cof@hotmail.com>
2018-10-16 12:33:21 +0100

    Fixed interface

--- MyInterface.js
+++ MyInterface.js
@@ -57,13 +57,6 @@ class MyInterface extends CGFinterface {
         var group = this.gui.addFolder("Cameras");
         group.open();
 
-        for (var i=0; i < Object.keys(views).length; i++) {
-
-            this.scene.cameraValues[Object.keys(views)[i]] = false;
-
-            group.add(this.scene.cameraValues, Object.keys(views)[i]);
-        }
-
-        //this.scene.cameraValues[Object.keys(this.scene.cameraValues)[0]] = true;
+        group.add(this.scene, 'currentCamera', Object.keys(views) );
     }
 }
