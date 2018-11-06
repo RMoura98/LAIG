83caca1fc46eecce2d217b182ac0395ff4ba8d8a
Unknown <up201604912@fe.up.pt>
2018-11-06 23:50:40 +0000

    ainda falta coisas nas linearAnim e as circulares ainda 20%

--- proj2/CircularAnimation.js
+++ proj2/CircularAnimation.js
@@ -4,60 +4,74 @@ class CircularAnimation extends Animation {
     * @constructor
     */
 
-    constructor(span, center, radius, startang, rotang) {
+	constructor(span, center, radius, startang, rotang) {
 
-        super("circular");
+		super("circular");
 
-        this.animationSpan = span;
+		this.animationSpan = span;
 		this.animationCenter = center;
-		console.log(this.animationCenter[0]);
-        this.animationRadius = radius;
+		this.animationRadius = radius;
 		this.animationStartang = startang; // Ângulo Inicial graus
-        this.animationRotang = rotang; // Ângulo de rotação graus
+		this.animationRotang = rotang; // Ângulo de rotação graus
 
-        this.hasEnded = false;
+		this.TSum = 0;
+		this.hasEnded = false;
 
-        console.log(Math.PI);
+		this.vel = (DEGREE_TO_RAD * this.animationRotang) / this.animationSpan;	//nao esta bem
+	}
 
-    }
+	getMatrix(time) {
 
-    getMatrix(time) {
-
-        var animationMat = mat4.create();       
+		var animationMat = mat4.create();
 		mat4.identity(animationMat);
-		
-		let ang = this.startang + this.angularSpeed * time;
 
-        mat4.translate(animationMat, animationMat, [this.animationCenter[0], this.animationCenter[1], this.animationCenter[2]]);
-        mat4.rotate(animationMat, animationMat, ang, [0, 1, 0]); 		//rotacao eixo vertical (a unica rotacao possivel)
-        mat4.translate(animationMat, animationMat, [this.animationRadius, 0, 0]);
+		var ang;
+
+		this.TSum += time;
+		if (this.TSum > this.animationSpan) {
+			let ans = this.TSum - this.animationSpan;
+			var ans2 = time - ans;
+			ang = this.animationStartang + this.vel * ans2;
+			this.hasEnded = true;
+		}
+		else {
+			ang = this.animationStartang + this.vel * time;
+		}
+
+		mat4.translate(animationMat, animationMat, [this.animationCenter[0], this.animationCenter[1], this.animationCenter[2]]);
+		mat4.rotate(animationMat, animationMat, DEGREE_TO_RAD * ang, [0, 1, 0]);
+		//mat4.translate(animationMat, animationMat, [this.animationRadius,0,0] );
+		mat4.translate(animationMat, animationMat, [-this.animationCenter[0], -this.animationCenter[1], -this.animationCenter[2]]);
+
+		/* mat4.translate(animationMat, animationMat, [-this.animationCenter[0], -this.animationCenter[1], -this.animationCenter[2]]);
+		mat4.rotate(animationMat, animationMat, ang, [0, 1, 0]);
+		mat4.translate(animationMat, animationMat, [this.animationCenter[0], this.animationCenter[1], this.animationCenter[2]]); */
 
 
-       
-        return animationMat;
-    }
+		return animationMat;
+	}
 
-    updateValues() {
+	updateValues() {
 
-       /*  if( (this.CPTransition + 1) >= this.controlPoints.length)
-            this.hasEnded = true;
-        else {
+		/*  if( (this.CPTransition + 1) >= this.controlPoints.length)
+			 this.hasEnded = true;
+		 else {
+ 
+			 this.TperCPSum = 0;
+ 
+			 this.velocityX = Math.pow(this.controlPoints[this.CPTransition+1][0] - this.controlPoints[this.CPTransition][0], 2) / this.TperCP;
+ 
+			 this.velocityY = Math.pow(this.controlPoints[this.CPTransition+1][1] - this.controlPoints[this.CPTransition][1], 2) / this.TperCP;
+ 
+			 this.velocityZ = Math.pow(this.controlPoints[this.CPTransition+1][2] - this.controlPoints[this.CPTransition][2], 2) / this.TperCP;
+ 
+			 this.CPTransition++;
+ 
+		 } */
+	}
 
-            this.TperCPSum = 0;
+	clone() {
+		return new CircularAnimation(this.animationSpan, this.animationCenter, this.animationRadius, this.animationStartang, this.animationRotang);
+	}
 
-            this.velocityX = Math.pow(this.controlPoints[this.CPTransition+1][0] - this.controlPoints[this.CPTransition][0], 2) / this.TperCP;
-
-            this.velocityY = Math.pow(this.controlPoints[this.CPTransition+1][1] - this.controlPoints[this.CPTransition][1], 2) / this.TperCP;
-
-            this.velocityZ = Math.pow(this.controlPoints[this.CPTransition+1][2] - this.controlPoints[this.CPTransition][2], 2) / this.TperCP;
-
-            this.CPTransition++;
-
-        } */
-    }
-
-    clone() {
-        return new CircularAnimation(this.animationSpan = span, this.animationCenter, this.animationRadius, this.animationStartang = startang, this.animationRotang);
-    }    
-   
 }
