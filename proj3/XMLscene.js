3c64e40314b7e01c7e019398e11ae9755e14722c
RMoura98 <ricardo.sanfinsmoura@gmail.com>
2018-12-11 13:28:12 +0000

    funcoes para o server

--- proj3/XMLscene.js
+++ proj3/XMLscene.js
@@ -122,6 +122,33 @@ class XMLscene extends CGFscene {
         this.sceneInited = true;
     }
 
+    //para o server inicio
+    getPrologRequest(requestString, onSuccess, onError, port)
+    {
+        var requestPort = port || 8081
+        var request = new XMLHttpRequest();
+        request.open('GET', 'http://localhost:'+requestPort+'/'+requestString, true);
+
+        request.onload = onSuccess || function(data){console.log("Request successful. Reply: " + data.target.response);};
+        request.onerror = onError || function(){console.log("Error waiting for response");};
+
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+        request.send();
+    }
+		
+    makeRequest(requestString)
+    {
+        // Make Request
+        this.getPrologRequest(requestString, this.handleReply);
+    }
+    
+    //Handle the Reply
+    handleReply(data){
+        console.log(data);
+    }
+
+    //para o server fim
+
     logPicking() {
         if (this.pickMode == false) {
             if (this.pickResults != null && this.pickResults.length > 0) {
